e8253b3e51a6aff281a03781e9eb1ac1351b7231
KaihuaS <shi.ka@husky.neu.edu>
2021-05-06 23:24:50 -0400

    rest api

---
+++ .pycharm_helpers/python_stubs/cache/61ea2f8f2fa1a9e9817cc5d32fcaaaa87e5c34403af9557ede4c710edee83716/_codecs_jp.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+# module _codecs_jp
+# from /usr/lib/python3.6/lib-dynload/_codecs_jp.cpython-36m-x86_64-linux-gnu.so
+# by generator 1.147
+# no doc
+# no imports
+
+# functions
+
+def getcodec(*args, **kwargs): # real signature unknown
+    pass
+
+# no classes
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x7feddad66978>'
+
+__map_cp932ext = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7feddac7b060>'
+
+__map_jisx0208 = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c381b0>'
+
+__map_jisx0212 = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c38180>'
+
+__map_jisx0213_1_bmp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c382d0>'
+
+__map_jisx0213_1_emp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c38e10>'
+
+__map_jisx0213_2_bmp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c38c30>'
+
+__map_jisx0213_2_emp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c38e40>'
+
+__map_jisx0213_bmp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c38d80>'
+
+__map_jisx0213_emp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c38ea0>'
+
+__map_jisx0213_pair = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c38e70>'
+
+__map_jisxcommon = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fedd9c382a0>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='_codecs_jp', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x7feddad66978>, origin='/usr/lib/python3.6/lib-dynload/_codecs_jp.cpython-36m-x86_64-linux-gnu.so')"
+
